16ad09b0d72e793cbedef12697ecb2e35320c539
Hamit YAY <hamityay@hotmail.com>
2016-08-18 14:47:39 +0300

    Added infos to seed.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating seed data...'
+
+p1 = Book.create(name: 'Suç ve Ceza', topic: 'Polisiye', year: Date.yesterday)
+p2 = Book.create(name: 'Savaş ve Barış', topic: 'Polisiye', year: Date.yesterday)
+p3 = Book.create(name: '9. Hariciye Koğuşu', topic: 'Polisiye', year: Date.yesterday)
+
+
+puts 'Creaed seed data ...'
