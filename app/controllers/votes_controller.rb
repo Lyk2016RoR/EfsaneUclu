d9cc59ac47ee4938a9240fc2923115102992ac12
Cagatay <zkylab@gmail.com>
2016-08-19 12:09:41 +0300

    added votes.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,21 +1,24 @@
 class VotesController < ApplicationController
 
-    before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_book
   before_action :set_vote, only: [:update]
   before_action :authorize_user!, only: [:update]
 
-  def create
-  @vote = @book.votes.new
-  @vote.rating = params[:vote][:rating]
-  @vote.user = current_user
 
-  if @vote.save
-    redirect_to @book, notice: "Vote was saved."
-  else
-    redirect_to @book, notice: "Vote is not valid."
-  end
-  end
+      def create
+          @vote = @book.votes.new
+          @vote.rating = params[:vote][:rating]
+          @vote.user = current_user
+
+            if @vote.save
+              redirect_to @book, notice: "Vote was saved."
+            else
+              redirect_to @book, notice: "Vote is not valid."
+            end
+      end
+
+
 
   def update
   if @vote.update(rating: params[:vote][:rating])
@@ -38,25 +41,5 @@ class VotesController < ApplicationController
   def set_book
   @book = Book.find(params[:book_id])
   end
-  end
 
-    before_action :set_book
-    before_action :set_vote, only: [:update]
-
-
-    def create
-
-    end
-
-
-
-private
-
-  def set_vote
-    @vote = Vote.find(params[:id])
-  end
-
-  def set_book
-    @book = Book.find(params[:book_id])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :show]
   resources :authors
 
+  resources :books do
+    resources :votes, only: [:create, :update]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
