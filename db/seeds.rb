a349068e663c19f8e813e84e59f6c5e60b4608d7
Hamit YAY <hamityay@hotmail.com>
2016-08-20 19:30:52 +0300

    Updated seed.rb file. Added book images and Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 puts 'Creating seed data...'
 
+Admin.create(email: "admin@admin.com", password: 123456, first_name: "Admin", last_name: "admin")
+
 c1 = Category.create(name: 'Science Fiction')
 c2 = Category.create(name: 'Action')
 c3 = Category.create(name: 'Political')
@@ -27,9 +29,9 @@ a4 = Author.create name: 'Author abcd', info: 'Info about author abcd.', publish
 a5 = Author.create name: 'Next Author', info: 'Info about next author.', publisher_id: p3.id
 a6 = Author.create name: 'Seventh Author', info: 'Info about seventh author.', publisher: p4
 
-b1 = Book.create(name: 'Suç ve Ceza', topic: 'Polisiye', year: Date.yesterday, summary: 'This is a summary about Suç ve Ceza', category_id: c4.id, author_ids: [3])
-b2 = Book.create(name: 'Savaş ve Barış', topic: 'Polisiye', year: Date.yesterday, summary: 'Summary of Savaş ve Barış', category: c3, author_ids: [1, 4])
-b3 = Book.create(name: '9. Hariciye Koğuşu', topic: 'Polisiye', year: Date.yesterday, summary: 'This is a summary about 9. Hariciye Koğuşu', category: c2, author_ids: [2, 6])
+b1 = Book.create(name: 'Suç ve Ceza', topic: 'Polisiye', year: Date.yesterday, summary: 'This is a summary about Suç ve Ceza', category_id: c4.id, photo_file_name: "distant-lights-linux-wallpaper.jpg", photo_content_type: "image/jpeg", photo_file_size: 269700, author_ids: [3])
+b2 = Book.create(name: 'Savaş ve Barış', topic: 'Polisiye', year: Date.yesterday, summary: 'Summary of Savaş ve Barış', category: c3, photo_file_name: "distant-lights-linux-wallpaper.jpg", photo_content_type: "image/jpeg", photo_file_size: 269700, author_ids: [1, 4])
+b3 = Book.create(name: '9. Hariciye Koğuşu', topic: 'Polisiye', year: Date.yesterday, summary: 'This is a summary about 9. Hariciye Koğuşu', category: c2, photo_file_name: "distant-lights-linux-wallpaper.jpg", photo_content_type: "image/jpeg", photo_file_size: 269700, author_ids: [2, 6])
 
 
 
